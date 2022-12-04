4450b44581f5f98273cdf6d4aa3d6aa3eab5b5fd
Moustafa Shaaban <Moustafa_Shaaban@zohomail.com>
2022-12-04 03:05:51 +0200

    Finished Version 2

--- README.md
+++ README.md
@@ -59,6 +59,27 @@ The data used in this project is downloaded from the [Seattle City Open Data](ht
 ``` python manage.py migrate seattle ```
 ``` python manage.py createsuperuser ```
 
-* Create a superuser: ``` python manage.py createsuperuser ```
+* Create a super user: ``` python manage.py createsuperuser ```
+
+* Login to the admin site with your super user and add the data in 'seattle/data' folder using the import functionality in each model.
 
 * Run the project: ``` python manage.py runserver ```
+
+------------------------------------------------------------------------------------------------------------
+
+# Version 2:
+
+In this version I improved the code by combining all the data in one Django Function View to show it in one map.
+
+Added Layer Control functionality to switch between different data layers.
+
+Changed the default style of the Markers and used font awesome icons:
+
+    * ` h-square ` icon for Hospitals Layer.
+    * ` graduation-cap ` icon for Public Schools Layer.
+    * ` book ` icon for Public Libraries Layer.
+
+
+### Version 2 Preview
+
+* [Youtube](https://www.youtube.com/watch?v=eU8r5l9-6JE)
--- Version_2/backend/seattle/views.py
+++ Version_2/backend/seattle/views.py
@@ -12,11 +12,9 @@ class HomePage(generic.TemplateView):
     template_name = 'index.html'
 
 
+def index(request):
 
-def schools_simple_markers(request):
-    """Function returns simple markers using Folium."""
-    schools = School.objects.all()
-
+    # Create folium basemap
     map = folium.Map(
         location=[47.6062100, -122.3320700],
         tiles='cartodbpositron',
@@ -24,41 +22,25 @@ def schools_simple_markers(request):
         attr= 'Public Schools in Seattle'
     )
 
-    # Add Full screen functionality to the map
-    Fullscreen().add_to(map)
-    LocateControl().add_to(map)
-    Geocoder().add_to(map)
-    folium.LatLngPopup().add_to(map)
+    # Add a second TileLayer to the basemap
+    folium.TileLayer('cartodbdark_matter').add_to(map)
 
-    # Loop through each data row in the database table:
+    
+    # Add Schools Data
+    schools = School.objects.all()
+
+    schools_simple_markers = folium.FeatureGroup(name='Schools Simple Markers').add_to(map)
+
     for school in schools:
         locations = [school.latitude, school.longitude]
         folium.Marker(
             locations,
-            icon=folium.Icon(icon = "book", prefix='fa'),
+            icon=folium.Icon(icon = "graduation-cap", prefix='fa'),
             tooltip="School Name: " + str(school.name),
             popup="School Address :" + school.address,
-        ).add_to(map)
+    ).add_to(schools_simple_markers)
 
-    map = map._repr_html_()
-
-    context = {
-        'map': map
-    }
-
-    return render(request, 'seattle/schools/simple_markers.html', context)
-
-
-def schools_marker_cluster(request):
-    """Function returns marker cluster using Folium."""
-    schools = School.objects.all()
-
-    map = folium.Map(
-        location=[47.6062100, -122.3320700],
-        tiles='cartodbpositron',
-        zoom_start=11,
-        attr='Public Schools in Seattle'
-    )
+    schools_marker_cluster = folium.FeatureGroup(name='Schools Marker Cluster', show=False).add_to(map)
 
     marker_cluster = MarkerCluster()
 
@@ -67,117 +49,19 @@ def schools_marker_cluster(request):
         marker_cluster.add_child(
             folium.Marker(
                 locations,
+                icon=folium.Icon(icon = "graduation-cap", prefix='fa'),
                 tooltip="School Name: " + str(school.name),
                 popup="School Address :" + school.address,
             )
-        )
-
-    map.add_child(marker_cluster)
+        ).add_to(schools_marker_cluster)
     
-    Fullscreen().add_to(map)
-    LocateControl().add_to(map)
-    Geocoder().add_to(map)
-    folium.LatLngPopup().add_to(map)
+    ##################################################################################################
 
-    map = map._repr_html_()
-
-    context = {'map': map}
-
-    return render(request, 'seattle/schools/marker_cluster.html', context)
-
-
-def hospitals_simple_markers(request):
-    """Function returns simple markers using Folium."""
-    hospitals = Hospital.objects.all()
-
-    map = folium.Map(
-        location=[47.6062100, -122.3320700],
-        tiles='cartodbpositron',
-        zoom_start=11,
-        attr= 'Public Schools in Seattle'
-    )
-
-    # Add Full screen functionality to the map
-    Fullscreen().add_to(map)
-    LocateControl().add_to(map)
-    Geocoder().add_to(map)
-    folium.LatLngPopup().add_to(map)
-
-    # Loop through each data row in the database table:
-    for hospital in hospitals:
-        locations = [hospital.latitude, hospital.longitude]
-        folium.Marker(
-            locations,
-            icon=folium.Icon(icon_color = "red", icon = "h-square", prefix='fa'),
-            tooltip="Hospital Name: " + str(hospital.facility),
-            popup="Hospital Address :" + hospital.address,
-        ).add_to(map)
-
-
-    map = map._repr_html_()
-
-    context = {
-        'map': map
-    }
-
-    return render(request, 'seattle/hospitals/simple_markers.html', context)
-
-
-def hospitals_marker_cluster(request):
-    """Function returns marker cluster using Folium."""
-    hospitals = Hospital.objects.all()
-
-    map = folium.Map(
-        location=[47.6062100, -122.3320700],
-        tiles='cartodbpositron',
-        zoom_start=11,
-        attr='Public Schools in Seattle'
-    )
-
-    marker_cluster = MarkerCluster()
-
-    for hospital in hospitals:
-        locations = [hospital.latitude, hospital.longitude]
-        marker_cluster.add_child(
-            folium.Marker(
-                locations,
-                tooltip="Hospital Name: " + str(hospital.facility),
-                popup="Hospital Address :" + hospital.address,
-            )
-        )
-
-    map.add_child(marker_cluster)
-
-    Fullscreen().add_to(map)
-    LocateControl().add_to(map)
-    Geocoder().add_to(map)
-    folium.LatLngPopup().add_to(map)
-
-    map = map._repr_html_()
-
-    context = {'map': map}
-
-    return render(request, 'seattle/hospitals/marker_cluster.html', context)
-
-
-def libraries_simple_markers(request):
-    """Function returns simple markers using Folium."""
+    # Add Libraries data:
     libraries = Library.objects.all()
 
-    map = folium.Map(
-        location=[47.6062100, -122.3320700],
-        tiles='cartodbpositron',
-        zoom_start=11,
-        attr= 'Public Schools in Seattle'
-    )
+    libraries_simple_markers = folium.FeatureGroup(name='Libraries Simple Markers', show=False).add_to(map)
 
-    # Add Full screen functionality to the map
-    Fullscreen().add_to(map)
-    LocateControl().add_to(map)
-    Geocoder().add_to(map)
-    folium.LatLngPopup().add_to(map)
-
-    # Loop through each data row in the database table:
     for library in libraries:
         locations = [library.latitude, library.longitude]
         folium.Marker(
@@ -185,28 +69,9 @@ def libraries_simple_markers(request):
             icon=folium.Icon(icon = "book", prefix='fa'),
             tooltip="Library Name: " + str(library.name),
             popup="Library Address :" + library.address,
-        ).add_to(map)
+        ).add_to(libraries_simple_markers)
 
-
-    map = map._repr_html_()
-
-    context = {
-        'map': map
-    }
-
-    return render(request, 'seattle/schools/simple_markers.html', context)
-
-
-def libraries_marker_cluster(request):
-    """Function returns marker cluster using Folium."""
-    libraries = Library.objects.all()
-
-    map = folium.Map(
-        location=[47.6062100, -122.3320700],
-        tiles='cartodbpositron',
-        zoom_start=11,
-        attr='Public Schools in Seattle'
-    )
+    libraries_marker_cluster = folium.FeatureGroup(name='Libraries Marker Cluster', show=False).add_to(map)
 
     marker_cluster = MarkerCluster()
 
@@ -215,14 +80,45 @@ def libraries_marker_cluster(request):
         marker_cluster.add_child(
             folium.Marker(
                 locations,
+                icon=folium.Icon(icon = "book", prefix='fa'),
                 tooltip="Library Name: " + str(library.name),
                 popup="Library Address :" + library.address,
             )
-        )
+        ).add_to(libraries_marker_cluster)
+    
+    ##################################################################################################
 
-    map.add_child(marker_cluster)
+    # Add Hospitals data
+    hospitals = Hospital.objects.all()
 
-    # Add Some Folium plugins to the map
+    hospitals_simple_markers = folium.FeatureGroup(name='Hospitals Simple Markers', show=False).add_to(map)
+
+    for hospital in hospitals:
+        locations = [hospital.latitude, hospital.longitude]
+        folium.Marker(
+            locations,
+            icon=folium.Icon(icon_color = "red", icon = "h-square", prefix='fa'),
+            tooltip="Hospital Name: " + str(hospital.facility),
+            popup="Hospital Address :" + hospital.address,
+        ).add_to(hospitals_simple_markers)
+
+    hospitals_marker_cluster = folium.FeatureGroup(name='Hospitals Marker Cluster', show=False).add_to(map)
+
+    marker_cluster = MarkerCluster()
+
+    for library in libraries:
+        locations = [library.latitude, library.longitude]
+        marker_cluster.add_child(
+            folium.Marker(
+                locations,
+                icon=folium.Icon(icon_color = "red", icon = "h-square", prefix='fa'),
+                tooltip="Hospital Name: " + str(hospital.facility),
+                popup="Hospital Address :" + hospital.address,
+            )
+        ).add_to(hospitals_marker_cluster)
+    ##################################################################################################
+
+    folium.LayerControl(position='bottomright').add_to(map)
     Fullscreen().add_to(map)
     LocateControl().add_to(map)
     Geocoder().add_to(map)
@@ -230,6 +126,8 @@ def libraries_marker_cluster(request):
 
     map = map._repr_html_()
 
-    context = {'map': map}
+    context = {
+        'map': map
+    }
 
-    return render(request, 'seattle/libraries/marker_cluster.html', context)
+    return render(request, 'map.html', context)
